c5749569e0b9b4104e7a7a739bf493071b8aa7f5
sandpitt-turtle <wottkowicz@gmail.com>
2025-01-28 15:10:38 -0500

    updates thru 'Use the Data in React'

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import { puppyList } from './data.js';
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
+
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Puppy List</h1>
+      <ul>
+        {puppies.map((puppy) => (
+          <li key={puppy.id}>{puppy.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
